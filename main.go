5e60f4a7d854ad4a7b4cd7ec8ebbb4d98416327f
ocionejr <ocionejunior@gmail.com>
2022-07-27 17:06:05 -0300

    feat: http routes

--- main.go
+++ main.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"github.com/ocionejr/go-gin-alura/database"
-	"github.com/ocionejr/go-gin-alura/models"
 	"github.com/ocionejr/go-gin-alura/routes"
 )
 
 func main() {
 	database.ConectaComBancoDeDados()
-	models.Alunos = [] models.Aluno{
-		{Nome: "Ocione", CPF: "312313123", RG: "312331323"},
-		{Nome: "Leo", CPF: "312313123", RG: "312331323"},
-	}
 	routes.HandleRequests()
 }
