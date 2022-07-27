b71e09bd556f4e985c43def3245908e49d57e860
ocionejr <ocionejunior@gmail.com>
2022-07-27 14:53:17 -0300

    feat: struct, database and gorm

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/ocionejr/go-gin-alura/database"
 	"github.com/ocionejr/go-gin-alura/models"
 )
 
@@ -14,4 +17,16 @@ func Saudacao(c *gin.Context){
 	c.JSON(200, gin.H{
 		"Api diz:": "E ai " + nome + ", tudo beleza?",
 	})
+}
+
+func CriaNovoAluno(c *gin.Context) {
+	var aluno models.Aluno
+	if err := c.ShouldBindJSON(&aluno); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	database.DB.Create(&aluno)
+	c.JSON(http.StatusOK, aluno)
 }
